e102aca21e366308a8467f7af621c72777661955
salemi.tahereh <salemi.tahereh@surenacs.com>
2023-10-30 08:14:52 +0330

    add product line

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
 
-export default function Home() {
+
+export default function Home({products}) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -15,8 +17,19 @@ export default function Home() {
         {/* Banner */}
         <Banner/>
         {/* product feed */}
+        <ProductFeed products={products}/>
+ 
       </main>    
   
     </div>
   );
+}
+export async function getServerSideProps(context){
+  const products=await  fetch("https://fakestoreapi.com/products").then((res)=>res.json());
+  return {
+    props:{
+      products,
+    }
+  }
+
 }
